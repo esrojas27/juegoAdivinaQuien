fa6c9e6453a5635f5bc4cc0fae8793cbefc60101
esrojas27 <62344298+esrojas27@users.noreply.github.com>
2020-09-27 13:28:23 -0500

    Segundo Commit

--- assets/js/gameScript.js
+++ assets/js/gameScript.js
@@ -2,16 +2,31 @@
 const mensajeNombre = localStorage.getItem('usuarios')
  
 usuariosDivididos = mensajeNombre.split(',')
-console.log("")
+
 nomCompleto=usuariosDivididos[0].split(':')
+
 apellidoCompleto=usuariosDivididos[1].split(':')
+
+puntajeCompleto=usuariosDivididos[2].split(':')
+
 nombreComillas=nomCompleto[1]
+
 apellidoComillas=apellidoCompleto[1]
+
+puntajeIncompleto=puntajeCompleto[1]
+
 nombreUsuario=nombreComillas.slice(1,-1)
+
 apellidoUsuario=apellidoComillas.slice(1,-1)
 
+puntajeUsuario=puntajeIncompleto.slice(0,1)
+
+
+console.log(puntajeUsuario)
+puntajeReal=parseInt(puntajeUsuario, 10)
 
 const mostrarNombre = document.getElementById('nombreJug').innerText = nombreUsuario + ' ' +apellidoUsuario;
+const mostrarPuntaje = document.getElementById('puntajeUsuario').innerText = puntajeReal;
 
 
 
@@ -34,6 +49,9 @@ adivinaQuien.addEventListener('click',(e)=>{
 
     if((inputRespuesta=="carlos" || inputRespuesta=="carlos alvarez") && descripcion=="me gusta programar css "){
         alert("Respuesta correcta")
+
+        puntajeReal=puntajeReal+1
+        mostrarPuntaje = document.getElementById('puntajeUsuario').innerText = puntajeReal;
     }
     else if((inputRespuesta=="andrea" || inputRespuesta=="andrea gamboa") && descripcion=="soy muy buena en el javascript "){
         alert("Respuesta correcta")
